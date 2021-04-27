c0bc93b5cc4010b8537e940854171086a59e9af0
raxkson <raxkson@gmail.com>
2021-04-27 16:38:16 +0900

    Database Test

--- utils/connect.php
+++ utils/connect.php
@@ -1,3 +1,3 @@
 <?php
-	$conn = mysqli_connect('192.168.55.14', 'user', 'root','database', 30007) or die('Error while connecting SQL: '.mysqli_error($conn));
+	$conn = mysqli_connect('10.10.0.143', 'kisec', 'kisec123','user1', 32001) or die('Error while connecting SQL: '.mysqli_error($conn));
 	//mysql_select_db('database', $conn) or die('Error while selecting database: '.mysql_error());
